b3a70506ab4a34442b6630f93d8f21b73b8a1686
HasinaBegumShaik <hasinabegum.shaik@gmail.com>
2022-03-31 02:13:27 +0530

    TestCasesCommit

---
+++ AssignmentTestProject/CreditControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CreditAPI.Controller;
+using CreditAPI.Logic;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+namespace CreditAPILogicTest
+{
+    public class CreditControllerTest
+    {     
+        [Fact]
+        public void Get_NegativeCreditTest()
+        {
+            var logic = new Mock<ICreditLogic>();
+            var controller = new CreditController(logic.Object);
+            var creditamount = -5000;
+            var exixtingcreditamount = -45000;
+            var term = 4;
+            var result = controller.creditDecision(creditamount, exixtingcreditamount, term);
+            Assert.IsType<BadRequestResult>(result);
+        }
+        [Fact]
+        public void Get_CreditDecisionTest()
+        {
+            var logic = new Mock<ICreditLogic>();
+            var controller = new CreditController(logic.Object);
+            var creditamount = 5000;
+            var existingamount = 25000;
+            var term = 3;
+            var result = controller.creditDecision(creditamount, existingamount, term);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
---
+++ AssignmentTestProject/CreditLogicTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CreditTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
